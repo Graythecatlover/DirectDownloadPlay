db75918dba9e86c43adba3de9fbe09f252934766
GrayTCL <100169016+Graythecatlover@users.noreply.github.com>
2023-10-29 16:38:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-// script.js
-function uploadVideo() {
-    // Get the selected file
-    var file = document.getElementById("file").files[0];
-    
-    // Create a FormData object and append the file to it
-    var data = new FormData();
-    data.append('file', file);
-    
-    // Send the request to the server
-    $.ajax({
-        url: 'upload_video.php',
-        type: 'POST',
-        data: data,
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function(data) {
-            console.log(data);
-            
-            // Update the page with the newly uploaded video
-            $('#player').empty().append(`
-                <video controls>
-                    <source src="${data}" type="video/mp4">
-                </video>
-            `);
+$(document).ready(function() {
+    // Get reference to the video element
+    var video = document.querySelector('video');
+  
+    // Add event listener to the play button
+    $('#playButton').on('click', function() {
+        // Get the value from the url input field
+        var url = $('#urlInput').val();
+      
+        // Check if the url is valid
+        if (url) {
+            // Load the video into the video element
+            video.src = url;
+          
+            // Play the video
+            video.play();
+        } else {
+            alert("Please enter a valid URL");
         }
     });
-}
+});
